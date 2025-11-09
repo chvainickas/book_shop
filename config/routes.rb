6f454b4ae47e9eac2f6cb4ac94008c7d3885c2d5
chvainickas <chvainickas@gmail.com>
2025-11-09 10:45:33 +0000

    Add show action for individual books
    - View detailed information for each book
    - Display all book attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index]
+  resources :books, only: [:index, :show]
 
   # Reveal health status on /up
   get "up" => "rails/health#show", as: :rails_health_check
